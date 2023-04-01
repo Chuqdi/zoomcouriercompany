f1552ab4d3a20b25251ad81bf2e4ebefad8ad143
Chuqdi <103970327+Chuqdi@users.noreply.github.com>
2023-03-31 21:10:29 -0700

    dhdh

--- static/js/track_shipment.js
+++ static/js/track_shipment.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function(){
     let start_tracking_shipment = document.getElementById("start_tracking_shipment");
     let start_tracking_shipment_input = document.getElementById("start_tracking_shipment_input")
     start_tracking_shipment.addEventListener("click", function(e){
+      alert("Mififi");
       e.preventDefault();
       let value = start_tracking_shipment_input.value;
       if(value.length < 1){
